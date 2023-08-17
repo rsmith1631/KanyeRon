549338fe88a18ad0adb10a781c98b4a48d4da161
rsmith1631 <rsmith1631@gmail.com>
2023-08-16 20:39:33 -0700

    Kanye and Ron Quote Generator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KanyeRon;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace KanyeRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            Console.ReadLine();
+        }
+    }
+}
